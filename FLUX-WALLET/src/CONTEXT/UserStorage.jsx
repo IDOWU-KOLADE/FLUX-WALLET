25319b0095bddbd27bff3dcd0f9435b4a90a84e3
IDOWU-KOLADE <koladeid9@gmail.com>
2026-07-08 14:28:20 +0100

    Working on a better logout

--- FLUX-WALLET/src/CONTEXT/UserStorage.jsx
+++ FLUX-WALLET/src/CONTEXT/UserStorage.jsx
@@ -42,9 +42,14 @@ export function loginUser (username, password) {
   
 }
 export function logoutUser () {
-  const storage = getStorage();
+    const storage = getStorage();
+  const confirmLogout = confirm('Are you sure you want to logout?')
+if (confirmLogout) {
   storage.loggedInUser = null;
   SetStorage(storage);
+  navigate()
+} else {return null}
+
 }
 export const resetPassword = (username, newPassword) => {
   const storage = getStorage();
